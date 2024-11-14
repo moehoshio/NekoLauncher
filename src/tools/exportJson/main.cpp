6a13ddaa6b346fc1ac1160df27eae828f1bb0a18
moehoshio <132435834+moehoshio@users.noreply.github.com>
2024-11-14 13:00:59 +0900

    Improved the exportJson tool

--- src/tools/exportJson/main.cpp
+++ src/tools/exportJson/main.cpp
@@ -1,36 +1,76 @@
+#include "./exec.h"
+#include "../../include/nlohmann/json.hpp"
+
 #include <filesystem>
 #include <iostream>
 #include <string>
-#include <openssl/sha.h>
-#include "exec.h"
-// ./exportTool /raw/path /new/path exportV1.0.1.json
+#include <fstream>
+
+struct FileInfos {
+    std::string url, fileName, hash;
+};
+
+std::string inputDir;
+std::string host;
+
+std::vector<FileInfos> versionFiles;
+
+void processDir(const std::filesystem::path &dirPath) {
+    for (const auto &it : std::filesystem::directory_iterator(dirPath)) {
+        if (it.is_regular_file()) {
+            std::string fileName = exec::unifiedThePaths(std::filesystem::relative(it.path(), inputDir).string());
+            versionFiles.push_back({host + fileName,
+                                    fileName,
+                                    exec::hashFile(fileName)});
+        } else if (it.is_directory()) {
+            processDir(it);
+        }
+    }
+}
+
+// ./exportTool /to/path/ https://www.example.com/download exportV1.0.1.json
 int main(int argc, char *argv[]) {
     if (argc < 3) {
-        std::cerr << "Not enough parameters!\ne.g ./exportTool /raw/path/(old version file) /new/path/(new version file) exportV1.0.1.json(export file name)\n";
+        std::cerr << "Not enough parameters!\ne.g: ./exportTool /to/path (need to update files dir) https://example.com/download/ (url prefix) exportV1.0.1.json (export file name)\n";
         return -1;
     }
 
-    for (auto it : {argv[1], argv[2]}) {
-
-        if (!std::filesystem::is_directory(it) || !std::filesystem::exists(it)) {
-            std::cerr << "Not the correct folder path or not exists!\n";
-            return -2;
-        }
+    if (!std::filesystem::is_directory(argv[1]) || !std::filesystem::exists(argv[1])) {
+        std::cerr << "Not the correct folder path or not exists!\n";
+        return -2;
     }
-
-    struct FileInfos {
-        std::string fileName, hash;
+    host = argv[2];
+    inputDir = argv[1];
+    processDir(inputDir);
+    nlohmann::json outJson = {
+        {"title",""},
+        {"msg",""},
+        {"poster",""},
+        {"time",""},
+        {"resVersion",""},
+        {"update",{}}
     };
-    std::vector<FileInfos> oldVersionFiles, newVersionFiles;
-
-    for (auto it : std::filesystem::directory_iterator(argv[1])) {
-        if (it.is_regular_file()) {
-            oldVersionFiles.push_back({it.path().filename().string(), exec::hashFile(it.path().string())});
+    for (const auto& fileInfo : versionFiles) { 
+        outJson["update"].push_back({
+            {"url",fileInfo.url},
+            {"name",fileInfo.fileName},
+            {"hash",fileInfo.hash},
+            {"meta",{
+                {"hashAlgorithm",exec::mapAlgorithm(exec::hashs::Algorithm::sha256)},
+                {"multis",false},
+                {"temp",false},
+                {"randName",false},
+                {"absoluteUrl",true}
+            }}
+        });
         }
+    std::string fileName = (argc < 4)? "export.json": argv[3];
+    std::ofstream outFile(fileName);
+    if (!outFile.is_open())
+    {
+        std::cout<<"faild to open file : "<<fileName<<"\n";
+        return -3;
     }
-    for (auto it : std::filesystem::directory_iterator(argv[2])) {
-        if (it.is_regular_file()) {
-            newVersionFiles.push_back({it.path().filename().string(), exec::hashFile(it.path().string())});
-        }
-    }
+    outFile<<outJson;
+    outFile.close();
 }
