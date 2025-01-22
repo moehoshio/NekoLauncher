1985b04ca4aafed1302addfe4aa8da2b276f8fea
moehoshio <132435834+moehoshio@users.noreply.github.com>
2025-01-22 19:57:35 +0900

    Reordered the defines, moving the parts that are more likely to need editing towards the front.

--- src/include/nekodefine.hpp
+++ src/include/nekodefine.hpp
@@ -2,25 +2,10 @@
 
 constexpr const char *launcherMode = "minecraft"; // Option: `minecraft`. Used in the `launcher` function in core.hpp
 
-using uint_64 =  unsigned long long;
-using uint_32 = unsigned int;
-using uint_16 = unsigned short;
-
-using int_64 = long long;
-using int_32 = int;
-using int_16 = short;
-
-using uint = uint_32;
-
 // Import the logging module for nerr exceptions , in nerr.hpp
 #define nerrImpLoggerModeDefine true
 
-// Define a convenient macro for printing logs
-#define FN __FUNCTION__
-
-#define FI __FILE__
-
-#define LI __LINE__
+#define NetWorkAuthlibHostDefine "skin.example.org"
 
 // e.g {"api.example.com","api.example.org","" ...}
 #define NetWorkHostListDefine {"api.example.com","www.example.org"};
@@ -32,8 +17,24 @@ using uint = uint_32;
 //any string
 #define NekoLcCoreVersionDefine "v0.0.1"
 
-// If true, the version number defined by NekoLcCoreVersionDefine will be used; otherwise, the version number in the data/version file will be used.
+// If true, the version number defined by NekoLcCoreVersionDefine will be used; otherwise, attempt the version number in the data/version file will be used.
 #define UseNekoLcCoreVersionDefine false
 
 
-#define NetWorkAuthlibHostDefine "skin.example.org"
+
+using uint_64 =  unsigned long long;
+using uint_32 = unsigned int;
+using uint_16 = unsigned short;
+
+using int_64 = long long;
+using int_32 = int;
+using int_16 = short;
+
+using uint = uint_32;
+
+// Define a convenient macro for printing logs
+#define FN __FUNCTION__
+
+#define FI __FILE__
+
+#define LI __LINE__
