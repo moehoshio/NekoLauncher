b3f5dab6659aafb476d2ddc6d7d23510cde0e7d5
moehoshio <132435834+moehoshio@users.noreply.github.com>
2025-06-04 06:22:32 +0900

    Moved the logging module from the standalone `nlog` namespace to `neko::log` and set up a namespace alias.

--- src/include/neko/log/nlog.hpp
+++ src/include/neko/log/nlog.hpp
@@ -4,36 +4,40 @@
 #include <string>
 
 // neko log
-namespace nlog {
-    
+namespace nlog = neko::log;
 
-    // Use formatting strings, for example: ("main.cpp",1,"%s : %s , v%d","func" ,"hello", 1,1)
-    // Out the :  main.cpp line 1 : "func : hello , v1.1"
-    constexpr inline void Info(const char *file, unsigned int line, const char *format, const auto &...args) {
-        loguru::log(loguru::Verbosity_INFO, file, line, format, args...);
-    }
-    constexpr inline void Err(const char *file, unsigned int line, const char *format, const auto &...args) {
-        loguru::log(loguru::Verbosity_ERROR, file, line, format, args...);
-    }
-    constexpr inline void Warn(const char *file, unsigned int line, const char *format, const auto &...args) {
-        loguru::log(loguru::Verbosity_WARNING, file, line, format, args...);
-    }
-    constexpr inline void Debug(const char *file, unsigned int line, const char *format, const auto &...args) {
-        loguru::log(loguru::Verbosity_9, file, line, format, args...);
-    }
+namespace neko {
 
-    struct autoLog {
-        const char *file;
-        unsigned int line;
-        std::string name;
-        std::string startMsg;
-        std::string endMsg;
-        autoLog(const char *file, unsigned int line, std::string name, std::string startMsg = "Enter", std::string endMsg = "End") : file(file), line(line), name(name), startMsg(startMsg), endMsg(endMsg) {
-            Info(file, line, "%s : %s", this->name.c_str(), this->startMsg.c_str());
+    namespace log {
+
+        // Use formatting strings, for example: ("main.cpp",1,"%s : %s , v%d","func" ,"hello", 1,1)
+        // Out the :  main.cpp line 1 : "func : hello , v1.1"
+        constexpr inline void Info(const char *file, unsigned int line, const char *format, const auto &...args) {
+            loguru::log(loguru::Verbosity_INFO, file, line, format, args...);
         }
-        ~autoLog() {
-            Info(file, line, "%s : %s", this->name.c_str(), this->endMsg.c_str());
+        constexpr inline void Err(const char *file, unsigned int line, const char *format, const auto &...args) {
+            loguru::log(loguru::Verbosity_ERROR, file, line, format, args...);
         }
-    };
-    
-} // namespace nlog
+        constexpr inline void Warn(const char *file, unsigned int line, const char *format, const auto &...args) {
+            loguru::log(loguru::Verbosity_WARNING, file, line, format, args...);
+        }
+        constexpr inline void Debug(const char *file, unsigned int line, const char *format, const auto &...args) {
+            loguru::log(loguru::Verbosity_9, file, line, format, args...);
+        }
+
+        struct autoLog {
+            const char *file;
+            unsigned int line;
+            std::string name;
+            std::string startMsg;
+            std::string endMsg;
+            autoLog(const char *file, unsigned int line, std::string name, std::string startMsg = "Enter", std::string endMsg = "End") : file(file), line(line), name(name), startMsg(startMsg), endMsg(endMsg) {
+                Info(file, line, "%s : %s", this->name.c_str(), this->startMsg.c_str());
+            }
+            ~autoLog() {
+                Info(file, line, "%s : %s", this->name.c_str(), this->endMsg.c_str());
+            }
+        };
+
+    } // namespace log
+} // namespace neko
