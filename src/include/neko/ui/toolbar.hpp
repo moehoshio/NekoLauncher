91917964511d5c8751ce3b2829fbfba7efb77f61
moehoshio <132435834+moehoshio@users.noreply.github.com>
2025-06-08 08:54:38 +0900

    Made minor optimizations to the format of `toolbar.hpp`.

--- src/include/neko/ui/toolbar.hpp
+++ src/include/neko/ui/toolbar.hpp
@@ -1,34 +1,33 @@
-#include <QtWidgets/QWidget>
 #include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
 
 namespace neko::ui {
+    
     class ToolBar : public QToolBar {
         Q_OBJECT
     public:
-        ToolBar(QWidget *parent = nullptr) : QToolBar(parent) {
+        explicit ToolBar(QWidget *parent = nullptr) : QToolBar(parent) {
             setMovable(false);
             setFloatable(false);
-
             setAcceptDrops(true);
-
             setContextMenuPolicy(Qt::PreventContextMenu);
         }
-        inline bool event(QEvent *event) override {
+
+        bool event(QEvent *event) override {
             if (event->type() == QEvent::MouseButtonPress)
                 emit requestMoveWindow();
-
             return QToolBar::event(event);
         }
-        inline void dragEnterEvent(QDragEnterEvent *event) override {
+
+        void dragEnterEvent(QDragEnterEvent *event) override {
             event->acceptProposedAction();
         }
 
-        inline void dragMoveEvent(QDragMoveEvent *p_event) override {
-            p_event->acceptProposedAction();
+        void dragMoveEvent(QDragMoveEvent *event) override {
+            event->acceptProposedAction();
         }
+
     signals:
-        // Signal to request moving the window when the toolbar is clicked
-        // This is used in MainWindow
         void requestMoveWindow();
     };
 } // namespace neko::ui
