ffaf8dee05f00e3a23b0b939f1179559ddd04444
moehoshio <132435834+moehoshio@users.noreply.github.com>
2025-06-10 17:13:33 +0900

    Updated `InputDialog` by separating implementation and definition, making the code and methods more standardized and modern. Included the following additions and improvements: - Added `setupStyle`, `setupFont`, and `resizeItems` methods. - Refined `setLines`, consolidating into a single version. - Enhanced `hideInput` to ensure complete cleanup execution.

--- src/include/neko/ui/dialogs/inputDialog.hpp
+++ src/include/neko/ui/dialogs/inputDialog.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
+#include "neko/ui/theme.hpp"
 #include "neko/ui/uiMsg.hpp"
 
 #include "neko/ui/widgets/pixmapWidget.hpp"
-
-#include "neko/function/info.hpp"
 
 #include <QtWidgets/QDialogButtonBox>
 #include <QtWidgets/QGridLayout>
@@ -16,103 +15,37 @@
 #include <QtWidgets/QWidget>
 
 namespace neko::ui {
-    
-    struct InputDialog : public QWidget {
+
+    class InputDialog : public QWidget {
+    private:
+        PixmapWidget *poster;
         QGridLayout *gridLayout;
         QWidget *centralWidget;
-        PixmapWidget *poster;
         QVBoxLayout *centralWidgetLayout;
-        std::vector<QWidget *> lineWidgets;
-        std::vector<QMetaObject::Connection> conns;
-        QDialogButtonBox *dialogButton;
         QLabel *title;
         QLabel *msg;
+        QDialogButtonBox *dialogButton;
+        std::vector<QLineEdit *> lines;
+
+    public:
         InputDialog(QWidget *parent = nullptr);
 
-        inline void hideInput() {
-            this->hide();
-            for (const auto &it : conns) {
-                disconnect(it);
-            }
-            conns.clear();
-        }
+        void hideInput();
+        void showInput(const InputMsg &m);
 
-        inline void showInput(const InputMsg &m) {
-            this->show();
-            title->setText(m.title.c_str());
-            msg->setText(m.msg.c_str());
-            if (!m.poster.empty()) {
-                poster->setPixmap(m.poster.c_str());
-                poster->show();
-            } else {
-                poster->hide();
-            }
+        void setLines(const std::vector<std::string> &vec);
 
-            setLines(m.lines);
-            bool *did = new bool(false);
-            if (!m.callback)
-                return;
+        /**
+         * @brief Retrieve the current data from the input components.
+         *
+         * @return std::vector<std::string> A vector containing the input from each line.
+         * @note Lines without input will be represented as empty strings.
+         */
+        std::vector<std::string> getLines();
 
-            conns.push_back(connect(
-                dialogButton, &QDialogButtonBox::accepted, [=, this] {
-                    m.callback(true);
-                    disconnect(dialogButton);
-                    *did = true;
-                }));
-            conns.push_back(connect(
-                dialogButton, &QDialogButtonBox::rejected, [=, this] {
-                    m.callback(false);
-                    disconnect(dialogButton);
-                    *did = true;
-                }));
+        void resizeItems(int windowWidth, int windowHeight);
 
-            conns.push_back(connect(this, &QWidget::destroyed, [=](QObject *) {
-                if (!(*did))
-                    m.callback(false);
-            }));
-        }
-
-        inline void setLines(const std::vector<std::string> vec) {
-            std::vector<QWidget *> lines;
-            for (auto &it : vec) {
-                QLineEdit *line = new QLineEdit(centralWidget);
-                line->setMinimumHeight(50);
-                line->setPlaceholderText(it.c_str());
-                if (it == neko::info::translations(neko::info::lang.general.password)) {
-                    line->setEchoMode(QLineEdit::PasswordEchoOnEdit);
-                }
-                lines.push_back(line);
-            }
-            setLines(lines);
-        }
-
-        inline void setLines(const std::vector<QWidget *> &placeholder) {
-            for (auto it : lineWidgets) {
-                centralWidgetLayout->removeWidget(it);
-                it->hide();
-                delete it;
-            }
-            lineWidgets.clear();
-
-            for (size_t i = 0; i < placeholder.size(); ++i) {
-                lineWidgets.push_back(placeholder[i]);
-                centralWidgetLayout->addWidget(lineWidgets[i]);
-            }
-
-            centralWidgetLayout->addWidget(dialogButton);
-        }
-
-        inline std::vector<std::string> getLines() {
-            std::vector<std::string> vec;
-            for (auto it : lineWidgets) {
-
-                if (auto line = dynamic_cast<QLineEdit *>(it)) {
-                    if (!line->text().isEmpty()) {
-                        vec.push_back(line->text().toStdString());
-                    }
-                }
-            }
-            return vec;
-        }
+        void setupFont(QFont font, QFont titleFont);
+        void setupStyle(const Theme &theme);
     };
 } // namespace neko::ui
---
+++ src/neko/ui/dialogs/inputDialog.cpp
@@ -0,0 +1,177 @@
+#include "neko/ui/dialogs/inputDialog.hpp"
+
+#include "neko/ui/theme.hpp"
+#include "neko/ui/uiMsg.hpp"
+
+#include "neko/ui/widgets/pixmapWidget.hpp"
+
+#include "neko/function/info.hpp"
+
+namespace neko::ui {
+    InputDialog::InputDialog(QWidget *parent)
+        : QWidget(parent),
+          gridLayout(new QGridLayout()),
+          poster(new PixmapWidget(this)),
+          centralWidget(new QWidget()),
+          centralWidgetLayout(new QVBoxLayout(centralWidget)),
+          dialogButton(new QDialogButtonBox(centralWidget)),
+          title(new QLabel(centralWidget)),
+          msg(new QLabel(centralWidget)) {
+        gridLayout->setRowStretch(0, 3);
+        gridLayout->setColumnStretch(0, 3);
+        gridLayout->setRowStretch(1, 3);
+        gridLayout->setColumnStretch(1, 3);
+        gridLayout->setRowStretch(2, 3);
+        gridLayout->setColumnStretch(2, 3);
+        gridLayout->setColumnStretch(3, 3);
+        gridLayout->setColumnStretch(4, 3);
+
+        this->setLayout(gridLayout);
+        gridLayout->addWidget(centralWidget, 1, 2);
+
+        poster->lower();
+
+        centralWidget->setSizePolicy(QSizePolicy::Policy::Preferred, QSizePolicy::Policy::Preferred);
+        title->setAlignment(Qt::AlignCenter);
+        centralWidgetLayout->addWidget(title);
+        centralWidgetLayout->addWidget(msg);
+        centralWidgetLayout->setStretchFactor(msg, 0.5);
+        centralWidgetLayout->addSpacing(12);
+        centralWidget->setLayout(centralWidgetLayout);
+
+        dialogButton->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        dialogButton->setStandardButtons(QDialogButtonBox::Cancel | QDialogButtonBox::Ok);
+    }
+
+    void InputDialog::hideInput() {
+        this->hide();
+        poster->clearPixmap();
+        for (auto it : lines) {
+            centralWidgetLayout->removeWidget(it);
+            it->deleteLater();
+        }
+        lines.clear();
+        centralWidgetLayout->removeWidget(dialogButton);
+        disconnect(dialogButton);
+        disconnect(this, &QWidget::destroyed, nullptr, nullptr);
+    }
+
+    void InputDialog::showInput(const InputMsg &m) {
+        this->show();
+        title->setText(QString::fromStdString(m.title));
+        msg->setText(QString::fromStdString(m.msg));
+        if (!m.poster.empty())
+            (void)poster->setPixmap(std::string_view(m.poster));
+
+        if (m.lineText.empty()) {
+            setLines({neko::info::translations(neko::info::lang.general.input)});
+        } else {
+            setLines(m.lineText);
+        }
+        auto did = std::make_shared<bool>(false);
+        if (!m.callback)
+            return;
+
+        connect(
+            dialogButton, &QDialogButtonBox::accepted, [=, this, did] {
+                m.callback(true);
+                *did = true;
+                disconnect(dialogButton);
+            });
+        connect(
+            dialogButton, &QDialogButtonBox::rejected, [=, this, did] {
+                m.callback(false);
+                *did = true;
+                disconnect(dialogButton);
+            });
+
+        connect(this, &QWidget::destroyed, [=, did](QObject *) {
+            if (!(*did))
+                m.callback(false);
+        });
+    }
+
+    void InputDialog::setLines(const std::vector<std::string> &vec) {
+        for (auto &it : vec) {
+            QLineEdit *line = new QLineEdit(centralWidget);
+            line->setMinimumHeight(50);
+            line->setPlaceholderText(QString::fromStdString(it));
+            line->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+            if (it == neko::info::translations(neko::info::lang.general.password)) {
+                line->setEchoMode(QLineEdit::PasswordEchoOnEdit);
+            }
+            centralWidgetLayout->addWidget(line);
+            lines.push_back(line);
+        }
+        centralWidgetLayout->addWidget(dialogButton);
+    }
+    std::vector<std::string> InputDialog::getLines() {
+        std::vector<std::string> vec;
+        for (auto it : lines) {
+            vec.push_back(it->text().toStdString());
+        }
+        return vec;
+    }
+    void InputDialog::resizeItems(int windowWidth, int windowHeight) {
+        if (!isVisible())
+            return;
+        resize(windowWidth, windowHeight);
+        if (poster) {
+            poster->setGeometry(0, 0, windowWidth, windowHeight);
+        }
+
+        if (centralWidget) {
+            int cw = windowWidth * 0.5;
+            int ch = windowHeight * 0.5;
+            int cx = (windowWidth - cw) / 2;
+            int cy = (windowHeight - ch) / 2;
+            centralWidget->setGeometry(cx, cy, cw, ch);
+        }
+
+        gridLayout->setRowStretch(0, 3);
+        gridLayout->setColumnStretch(0, 3);
+        gridLayout->setRowStretch(1, 3);
+        gridLayout->setColumnStretch(1, 3);
+        gridLayout->setRowStretch(2, 3);
+        gridLayout->setColumnStretch(2, 3);
+        gridLayout->setColumnStretch(3, 3);
+        gridLayout->setColumnStretch(4, 3);
+    }
+
+    void InputDialog::setupFont(QFont font, QFont titleFont) {
+        this->setFont(font);
+        centralWidget->setFont(font);
+        msg->setFont(font);
+        dialogButton->setFont(font);
+        title->setFont(titleFont);
+    }
+    void InputDialog::setupStyle(const Theme &theme) {
+        centralWidget->setStyleSheet(
+            QString("QWidget { background-color: %1; }"
+                    "QLineEdit { background-color: %1; color: %2; border: 1px solid %3; padding: %4px; border-radius: %5; }")
+                .arg(QString::fromUtf8(theme.backgroundColor.data(), theme.backgroundColor.size()))
+                .arg(QString::fromUtf8(theme.textColor.data(), theme.textColor.size()))
+                .arg(QString::fromUtf8(theme.borderColor.data(), theme.borderColor.size()))
+                .arg(theme.padding)
+                .arg(QString::fromUtf8(theme.borderRadius.data(), theme.borderRadius.size())));
+
+        title->setStyleSheet(
+            QString("QLabel { color: %1; font-weight: bold; }")
+                .arg(QString::fromUtf8(theme.accentColor.data(), theme.accentColor.size())));
+        msg->setStyleSheet(
+            QString("QLabel { color: %1; }")
+                .arg(QString::fromUtf8(theme.textColor.data(), theme.textColor.size())));
+        dialogButton->setStyleSheet(
+            QString("QDialogButtonBox { background-color: %1; }"
+                    "QPushButton { color: %2; background-color: %3; border-radius: %4; padding: %5; }"
+                    "QPushButton:hover { background-color: %6; }"
+                    "QPushButton:disabled { background-color: %7; }")
+                .arg(QString::fromUtf8(theme.backgroundColor.data(), theme.backgroundColor.size()))
+                .arg(QString::fromUtf8(theme.textColor.data(), theme.textColor.size()))
+                .arg(QString::fromUtf8(theme.buttonColor.data(), theme.buttonColor.size()))
+                .arg(theme.borderRadius)
+                .arg(theme.padding)
+                .arg(QString::fromUtf8(theme.buttonHoverColor.data(), theme.buttonHoverColor.size()))
+                .arg(QString::fromUtf8(theme.buttonDisabledColor.data(), theme.buttonDisabledColor.size())));
+    }
+} // namespace neko::ui
