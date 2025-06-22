552ad4623ef09714d336aa39bc9d2c66439d355e
moehoshio <132435834+moehoshio@users.noreply.github.com>
2025-06-22 23:42:30 +0900

    Added `uchar`, `int8`, and `uint8` type aliases to `types.hpp`.

--- src/include/neko/schema/types.hpp
+++ src/include/neko/schema/types.hpp
@@ -5,15 +5,23 @@
 namespace neko {
 
     inline namespace types {
+        // Character and string types
         using cstr = const char *;
+        using uchar = unsigned char;
 
+        // Not implemented
+        // using str = neko::string;
+
+        // Fixed-width integer types
         using uint64 = std::uint64_t;
         using uint32 = std::uint32_t;
         using uint16 = std::uint16_t;
+        using uint8  = std::uint8_t;
 
         using int64 = std::int64_t;
         using int32 = std::int32_t;
         using int16 = std::int16_t;
+        using int8  = std::int8_t;
     } // namespace types
 
 } // namespace neko
