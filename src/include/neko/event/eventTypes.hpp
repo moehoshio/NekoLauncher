dfab184b08bd5a1e0954dbbda1196b9e356008f1
moehoshio <132435834+moehoshio@users.noreply.github.com>
2025-07-12 03:47:48 +0900

    Added several new event types to `event/eventTypes.hpp`, including:
    - `ShowHintEvent`
    - `ShowLoadEvent`
    - `UpdateLoadingEvent`
    - *(and others)*

--- src/include/neko/event/eventTypes.hpp
+++ src/include/neko/event/eventTypes.hpp
@@ -1,8 +1,25 @@
 #pragma once
 #include "neko/schema/types.hpp"
+#include "neko/ui/uiMsg.hpp"
 
 namespace neko::event {
 
     struct ExitEvent {};
 
+    struct ShowHintEvent : public neko::ui::HintMsg {
+        ShowHintEvent(const neko::ui::HintMsg &msg) : neko::ui::HintMsg(msg) {}
+    };
+
+    struct ShowLoadEvent : public neko::ui::LoadMsg {
+        ShowLoadEvent(const neko::ui::LoadMsg &msg) : neko::ui::LoadMsg(msg) {}
+    };
+
+    struct UpdateLoadingEvent {
+        std::string process;
+        neko::uint32 progressVal;
+    };
+
+    struct ShowInputEvent : public neko::ui::InputMsg {
+        ShowInputEvent(const neko::ui::InputMsg &msg) : neko::ui::InputMsg(msg) {}
+    };
 } // namespace neko::event
